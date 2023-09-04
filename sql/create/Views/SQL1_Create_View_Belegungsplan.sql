fdf257f201f7cf3fb9d76b46e452e5fe6247c089
DGrothe-PhD <dag_fs@t-online.de>
2023-09-04 18:00:27 +0200

    Unterrichtsart ergänzt

--- sql/create/Views/SQL1_Create_View_Belegungsplan.sql
+++ sql/create/Views/SQL1_Create_View_Belegungsplan.sql
@@ -1,22 +1,25 @@
 USE [Nachhilfe]
 GO
 
-/****** Object:  View [dbo].[Belegungsplan]    Script Date: 17.03.2023 19:30:36 ******/
+/****** Object:  View [dbo].[Belegungsplan]    Script Date: 04.09.2023 17:59:25 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE VIEW [dbo].[Belegungsplan]
+CREATE OR ALTER VIEW [dbo].[Belegungsplan]
 AS
-SELECT        dbo.tb_Schueler.Name, dbo.tb_Schueler.Vorname, dbo.tb_Faecher.Bezeichnung, dbo.tb_Faecher.Stufe, dbo.tb_Fachbelegung.Anfangsdatum, dbo.tb_Fachbelegung.Enddatum
-FROM            dbo.tb_Schueler INNER JOIN
-                         dbo.tb_Fachbelegung ON dbo.tb_Schueler.SchuelerID = dbo.tb_Fachbelegung.SchuelerID INNER JOIN
-                         dbo.tb_Faecher ON dbo.tb_Fachbelegung.FachID = dbo.tb_Faecher.FachID
+SELECT dbo.tb_Schueler.Name, dbo.tb_Schueler.Vorname, dbo.tb_Faecher.Bezeichnung, dbo.tb_Faecher.Stufe, dbo.tb_Fachbelegung.Anfangsdatum, dbo.tb_Fachbelegung.Enddatum, dbo.tb_Unterrichtsart.Bezeichnung AS Art, 
+                  dbo.tb_Unterrichtsart.OrtOderMedium
+FROM     dbo.tb_Schueler INNER JOIN
+                  dbo.tb_Fachbelegung ON dbo.tb_Schueler.SchuelerID = dbo.tb_Fachbelegung.SchuelerID INNER JOIN
+                  dbo.tb_Faecher ON dbo.tb_Fachbelegung.FachID = dbo.tb_Faecher.FachID INNER JOIN
+                  dbo.tb_Unterrichtsart ON dbo.tb_Fachbelegung.UnterrichtsartID = dbo.tb_Unterrichtsart.ArtID
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane1', @value=N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+IF NOT EXISTS (SELECT * FROM sys.fn_listextendedproperty(N'MS_DiagramPane1' , N'SCHEMA',N'dbo', N'VIEW',N'Belegungsplan', NULL,NULL))
+	EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane1', @value=N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
 Begin DesignProperties = 
    Begin PaneConfigurations = 
       Begin PaneConfiguration = 0
@@ -101,7 +104,7 @@ Begin DesignProperties =
             Begin Extent = 
                Top = 11
                Left = 263
-               Bottom = 172
+               Bottom = 241
                Right = 430
             End
             DisplayFlags = 280
@@ -111,8 +114,18 @@ Begin DesignProperties =
             Begin Extent = 
                Top = 18
                Left = 493
-               Bottom = 131
+               Bottom = 168
                Right = 660
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "tb_Unterrichtsart"
+            Begin Extent = 
+               Top = 151
+               Left = 647
+               Bottom = 292
+               Right = 851
             End
             DisplayFlags = 280
             TopColumn = 0
@@ -140,23 +153,188 @@ Begin DesignProperties =
       Begin ColumnWidths = 11
          Column = 1440
          Alias = 900
-         Table = 1170
+         Table = 1176
          Output = 720
          Append = 1400
          NewValue = 1170
-         SortType = 1350
-         SortOrder = 1410
+         SortType = 1356
+         SortOrder = 1416
          GroupBy = 1350
-         Filter = 1350
+         Filter = 1356
          Or = 1350
          Or = 1350
+       ' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'Belegungsplan'
+ELSE
+BEGIN
+	EXEC sys.sp_updateextendedproperty @name=N'MS_DiagramPane1', @value=N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+Begin DesignProperties = 
+   Begin PaneConfigurations = 
+      Begin PaneConfiguration = 0
+         NumPanes = 4
+         Configuration = "(H (1[40] 4[20] 2[20] 3) )"
+      End
+      Begin PaneConfiguration = 1
+         NumPanes = 3
+         Configuration = "(H (1 [50] 4 [25] 3))"
+      End
+      Begin PaneConfiguration = 2
+         NumPanes = 3
+         Configuration = "(H (1 [50] 2 [25] 3))"
+      End
+      Begin PaneConfiguration = 3
+         NumPanes = 3
+         Configuration = "(H (4 [30] 2 [40] 3))"
+      End
+      Begin PaneConfiguration = 4
+         NumPanes = 2
+         Configuration = "(H (1 [56] 3))"
+      End
+      Begin PaneConfiguration = 5
+         NumPanes = 2
+         Configuration = "(H (2 [66] 3))"
+      End
+      Begin PaneConfiguration = 6
+         NumPanes = 2
+         Configuration = "(H (4 [50] 3))"
+      End
+      Begin PaneConfiguration = 7
+         NumPanes = 1
+         Configuration = "(V (3))"
+      End
+      Begin PaneConfiguration = 8
+         NumPanes = 3
+         Configuration = "(H (1[56] 4[18] 2) )"
+      End
+      Begin PaneConfiguration = 9
+         NumPanes = 2
+         Configuration = "(H (1 [75] 4))"
+      End
+      Begin PaneConfiguration = 10
+         NumPanes = 2
+         Configuration = "(H (1[66] 2) )"
+      End
+      Begin PaneConfiguration = 11
+         NumPanes = 2
+         Configuration = "(H (4 [60] 2))"
+      End
+      Begin PaneConfiguration = 12
+         NumPanes = 1
+         Configuration = "(H (1) )"
+      End
+      Begin PaneConfiguration = 13
+         NumPanes = 1
+         Configuration = "(V (4))"
+      End
+      Begin PaneConfiguration = 14
+         NumPanes = 1
+         Configuration = "(V (2))"
+      End
+      ActivePaneConfig = 0
+   End
+   Begin DiagramPane = 
+      Begin Origin = 
+         Top = 0
+         Left = 0
+      End
+      Begin Tables = 
+         Begin Table = "tb_Schueler"
+            Begin Extent = 
+               Top = 6
+               Left = 38
+               Bottom = 188
+               Right = 205
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "tb_Fachbelegung"
+            Begin Extent = 
+               Top = 11
+               Left = 263
+               Bottom = 241
+               Right = 430
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "tb_Faecher"
+            Begin Extent = 
+               Top = 18
+               Left = 493
+               Bottom = 168
+               Right = 660
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "tb_Unterrichtsart"
+            Begin Extent = 
+               Top = 151
+               Left = 647
+               Bottom = 292
+               Right = 851
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+      End
+   End
+   Begin SQLPane = 
+   End
+   Begin DataPane = 
+      Begin ParameterDefaults = ""
+      End
+      Begin ColumnWidths = 9
+         Width = 284
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+      End
+   End
+   Begin CriteriaPane = 
+      Begin ColumnWidths = 11
+         Column = 1440
+         Alias = 900
+         Table = 1176
+         Output = 720
+         Append = 1400
+         NewValue = 1170
+         SortType = 1356
+         SortOrder = 1416
+         GroupBy = 1350
+         Filter = 1356
          Or = 1350
+         Or = 1350
+       ' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'Belegungsplan'
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.fn_listextendedproperty(N'MS_DiagramPane2' , N'SCHEMA',N'dbo', N'VIEW',N'Belegungsplan', NULL,NULL))
+	EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane2', @value=N'  Or = 1350
       End
    End
 End
 ' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'Belegungsplan'
+ELSE
+BEGIN
+	EXEC sys.sp_updateextendedproperty @name=N'MS_DiagramPane2', @value=N'  Or = 1350
+      End
+   End
+End
+' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'Belegungsplan'
+END
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPaneCount', @value=1 , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'Belegungsplan'
+IF NOT EXISTS (SELECT * FROM sys.fn_listextendedproperty(N'MS_DiagramPaneCount' , N'SCHEMA',N'dbo', N'VIEW',N'Belegungsplan', NULL,NULL))
+	EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPaneCount', @value=2 , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'Belegungsplan'
+ELSE
+BEGIN
+	EXEC sys.sp_updateextendedproperty @name=N'MS_DiagramPaneCount', @value=2 , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'Belegungsplan'
+END
 GO
 
