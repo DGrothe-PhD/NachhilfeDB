a4fd992fade5ba77d460b1bc0d9a2ffffd10946a
DGrothe-PhD <dag_fs@t-online.de>
2024-05-10 10:18:05 +0200

    filling table

---
+++ sql/create/SQL3B_Fill_TB_Schultypen_Unterrichtsart.sql
@@ -0,0 +1,23 @@
+USE [Nachhilfe]
+GO
+
+----
+
+INSERT INTO [dbo].[tb_Schultypen] VALUES ('Gymnasium', 'Mittelstufe')
+INSERT INTO [dbo].[tb_Schultypen] VALUES ('Gymnasium', 'Oberstufe')
+INSERT INTO [dbo].[tb_Schultypen] VALUES ('Realschule', '')
+INSERT INTO [dbo].[tb_Schultypen] VALUES ('Internat', '')
+INSERT INTO [dbo].[tb_Schultypen] VALUES ('Hauptschule', '')
+INSERT INTO [dbo].[tb_Schultypen] VALUES ('Förderschule', '')
+INSERT INTO [dbo].[tb_Schultypen] VALUES ('Grundschule', '')
+
+----
+
+INSERT INTO [dbo].[tb_Unterrichtsart] VALUES ('Browser', 'Senfcall')
+INSERT INTO [dbo].[tb_Unterrichtsart] VALUES ('Mobil', 'Videocall')
+INSERT INTO [dbo].[tb_Unterrichtsart] VALUES ('Online', 'Alfaview')
+INSERT INTO [dbo].[tb_Unterrichtsart] VALUES ('Online', 'MS Teams')
+INSERT INTO [dbo].[tb_Unterrichtsart] VALUES ('Online', 'Zoom')
+INSERT INTO [dbo].[tb_Unterrichtsart] VALUES ('VorOrt', 'Beim Schüler')
+
+----
