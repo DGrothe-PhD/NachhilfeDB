9eba6c57875b94eab9ed1bc79b36959d27ddf84a
DGrothe-PhD <dag_fs@t-online.de>
2025-03-08 14:04:55 +0100

    add count by Unterrichtsart

--- sql/queries/SQL_QueryByDayAndTime.sql
+++ sql/queries/SQL_QueryByDayAndTime.sql
@@ -1,9 +1,18 @@
 use Nachhilfe;
 Go
 
+select count(*) from tb_Unterricht
 
+-- nach Wochentag und Zeit
 select-- datepart(dw, Datum) i, 
 datename(dw, Datum) as 'Wochentag', Uhrzeit, count(*) Anzahl from tb_Unterricht
 group by datepart(dw, Datum),
 datename(dw, Datum), Uhrzeit
 order by datepart(dw, Datum)
+
+-- nach Unterrichtsart
+select tb_Unterrichtsart.Bezeichnung 'Medium', count(*) Anzahl 
+from tb_Unterricht
+left join tb_Fachbelegung on FachbelegungID = FachBelegID
+left join tb_Unterrichtsart on UnterrichtsartID = ArtID
+group by tb_Unterrichtsart.Bezeichnung
